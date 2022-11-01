cb8018e39aedbb6cd24634ddb8d71c655d405688
Tobias Falkenstein <tobias.falkenstein@bosch.com>
2022-11-01 10:33:52 +0100

    vtd: Add external ego model class

--- optional/vtd/src/actuator_component.hpp
+++ optional/vtd/src/actuator_component.hpp
@@ -26,7 +26,8 @@
 
 #include <memory>  // for shared_ptr<>
 
-#include <cloe/component/latlong_actuator.hpp>  // for LatLongActuator
+#include <cloe/component/latlong_actuator.hpp>     // for LatLongActuator
+#include <cloe/component/vehicle_state_model.hpp>  // for VehicleStateModel
 
 #include "task_control.hpp"  // for TaskControl
 #include "vtd_logger.hpp"    // for vtd_logger
@@ -157,21 +158,73 @@ class VtdLatLongActuator : public VtdVehicleControl, public cloe::LatLongActuato
   cloe::utility::ActuationLevel old_level_;
 };
 
+/**
+ * VtdExternalEgoModel provides the ego state from an external model to VTD.
+ *
+ * # Usage
+ *
+ * Every VTD cycle, the following needs to be done:
+ *
+ * - `step_begin` retrieves the updated ego vehicle state from the external model
+ *    and registers the new state with the TaskControl client.
+ * - `TaskControl::add_trigger_and_send` must be called to send the information
+ *   to VTD.
+ */
+class VtdExternalEgoModel : public VtdVehicleControl, public cloe::VehicleStateModel {
+ public:
+  VtdExternalEgoModel(std::shared_ptr<TaskControl> tc, uint64_t id, const std::string& veh_name)
+      : VehicleStateModel("vtd/ego_state")
+      , task_control_(tc)
+      , vehicle_id_(id)
+      , vehicle_name_(veh_name) {}
+  virtual ~VtdExternalEgoModel() = default;
+
+  /**
+   * Add the DynObjectState package to the TaskControl.
+   *
+   * This should only be called once per simulation step. This method will not
+   * pay attention for you.  Later, when the TaskControl sends its packages,
+   * this one will be part of it.
+   */
+  void step_begin(const cloe::Sync& sync) override {
+    if (this->is_vehicle_state()) {
+      add_dyn_object_state();
+    } else if (sync.step() > 1) {
+      // During the first time step, external model has not yet been updated. Throw otherwise.
+      throw cloe::ModelError("VtdExternalEgoModel: vehicle state not set.");
     }
   }
 
-  cloe::Duration process(const cloe::Sync& sync) override { return LatLongActuator::process(sync); }
-
   void reset() override {
-    old_level_.set_none();
-    LatLongActuator::reset();
+    VehicleStateModel::reset();
     task_control_->reset();
+  }
+
+  cloe::Json to_json() const override {
+    return dynamic_cast<const cloe::VehicleStateModel&>(*this);
+  }
+
+ private:
+  void add_dyn_object_state() {
+    auto ego_state = this->vehicle_state();
+    assert(ego_state);
+    DynObjectState os;
+    assert(static_cast<uint64_t>(ego_state->id) == vehicle_id_);
+    os.base_id = ego_state->id;
+    os.base_type = cloe_vtd_obj_class_map.at(ego_state->classification);
+    os.base_name = vehicle_name_;
+    os.base_geo = rdb_geometry_from_object(ego_state.get());
+    os.base_pos = rdb_coord_from_object(ego_state.get());
+    os.ext_speed = rdb_coord_from_vector3d(ego_state->velocity, ego_state->angular_velocity);
+    os.ext_accel = rdb_coord_pos_from_vector3d(ego_state->acceleration);
+    // Add new ego state to task control message.
+    task_control_->add_dyn_object_state(os);
   }
 
  private:
   std::shared_ptr<TaskControl> task_control_;
   uint64_t vehicle_id_;
-  cloe::utility::ActuationLevel old_level_;
+  std::string vehicle_name_;
 };
 
 }  // namespace vtd
