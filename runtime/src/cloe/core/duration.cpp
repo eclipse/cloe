a866f01f629172365f6474049a516c4522886b68
Benjamin Morgan <benjamin.morgan@bosch.com>
2023-05-10 14:01:44 +0200

    runtime: Add parse_duration() function

--- runtime/src/cloe/core/duration.cpp
+++ runtime/src/cloe/core/duration.cpp
@@ -22,12 +22,15 @@
 
 #include <cloe/core/duration.hpp>
 
-#include <chrono>  // for duration_cast
-#include <string>  // for operator+, string, to_string
+#include <chrono>    // for duration_cast
+#include <cmath>     // for pow
+#include <optional>  // for optional
+#include <string>    // for operator+, string, to_string
 
 namespace cloe {
 namespace {
 
+// to_string human readable
 std::string to_string_hr(double d) {
   auto n = std::to_string(d);
   n.erase(n.find_last_not_of('0') + 1, std::string::npos);
@@ -38,6 +41,64 @@ std::string to_string_hr(double d) {
 }
 
 }  // anonymous namespace
+
+Duration parse_duration(const std::string& fmt) {
+  // Get whole component of the duration
+  size_t idx = 0;
+  uint64_t whole = std::stoull(fmt, &idx);
+  if (idx >= fmt.size()) {
+    throw std::invalid_argument("number requires unit to parse");
+  }
+
+  // Get any fraction component of duration.
+  // Note the number of digits this number has, so we can efficiently
+  // add it the result without losing any precision.
+  uint64_t fraction = 0;
+  size_t fraction_digits = 0;
+  if (fmt[idx] == '.') {
+    auto prev = ++idx;
+    fraction = std::stoull(fmt.substr(idx), &idx);
+    fraction_digits = idx;
+    idx += prev;
+  }
+  if (idx >= fmt.size()) {
+    throw std::invalid_argument("number requires unit to parse");
+  }
+
+  // Read rest of fmt as unit, skipping whitespace between number
+  // and the unit.
+  while (fmt[idx] == ' ') {
+    idx++;
+  }
+  std::string unit = fmt.substr(idx);
+
+  Duration result(0);
+  int ns_exponent;
+  if (unit == "ns") {
+    ns_exponent = 0;
+    result += std::chrono::nanoseconds(whole);
+  } else if (unit == "us") {
+    ns_exponent = 3;
+    result += std::chrono::microseconds(whole);
+  } else if (unit == "ms") {
+    ns_exponent = 6;
+    result += std::chrono::milliseconds(whole);
+  } else if (unit == "s") {
+    ns_exponent = 9;
+    result += std::chrono::seconds(whole);
+  } else {
+    throw std::invalid_argument("unit not supported yet: " + unit);
+  }
+
+  if (fraction) {
+    int fraction_exponent = ns_exponent - fraction_digits;
+    if (fraction_exponent < 0) {
+      throw std::out_of_range("cannot represent sub-nanosecond precision: " + fmt);
+    }
+    result += std::chrono::nanoseconds(fraction) * static_cast<size_t>(std::pow(10, fraction_exponent));
+  }
+  return result;
+}
 
 std::string to_string(const Duration& ns) {
   auto count = ns.count();
