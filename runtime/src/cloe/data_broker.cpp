7640f3e903ce443959937719bcef68e486223cb9
Martin Henselmeyer <martin.henselmeyer@bosch.com>
2023-05-24 13:56:51 +0200

    runtime(databroker): Add initial DataBroker implementation

---
+++ runtime/src/cloe/data_broker.cpp
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2023 Robert Bosch GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+/**
+ * \file cloe/data_broker.cpp
+ * \see  cloe/data_broker.hpp
+ */
+
+#include <cloe/data_broker.hpp>
+
+namespace cloe {
+
+// Objective: Instantiate all templates for relevant datatypes
+
+// Remove 'extern' from template-instantiation macros
+#define extern
+// Invoke instantiation macros
+CLOE_DATABROKER_TEMPLATE_INSTANTATION_TYPES()
+// Cleanup preprocessor
+#undef extern
+
+}  // namespace cloe
