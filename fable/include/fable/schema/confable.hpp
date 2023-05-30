2162221b2a8f9d295403b470498c2dae6c7de587
Benjamin Morgan <benjamin.morgan@bosch.com>
2023-05-30 11:39:51 +0200

    fable: Enable serializing and deserializing into refs

--- fable/include/fable/schema/confable.hpp
+++ fable/include/fable/schema/confable.hpp
@@ -93,6 +93,10 @@ class FromConfable : public Base<FromConfable<T>> {
     return tmp;
   }
 
+  void serialize_into(Json& j, const Type& x) const { x.to_json(j); }
+
+  void deserialize_into(const Conf& c, Type& x) const { x.from_conf(c); }
+
   void reset_ptr() override {
     ptr_ = nullptr;
     schema_.reset_ptr();
