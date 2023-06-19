0f6def822896c315c5336c3649d02a2ba2707383
Benjamin Morgan <benjamin.morgan@bosch.com>
2023-06-19 12:04:29 +0200

    fable: Use string_view in string utility functions

--- fable/src/fable/utility/string.cpp
+++ fable/src/fable/utility/string.cpp
@@ -20,12 +20,13 @@
  * \see  fable/utility/string.hpp
  */
 
-#include <string>  // for string
-#include <vector>  // for vector<>
+#include <string>       // for string
+#include <string_view>  // for string_view
+#include <vector>       // for vector<>
 
 namespace fable {
 
-std::string join_vector(const std::vector<std::string>& v, const std::string& sep) {
+std::string join_vector(const std::vector<std::string>& v, std::string_view sep) {
   std::string result;
   size_t n = v.size();
   for (size_t i = 1; i <= n; i++) {
@@ -37,18 +38,29 @@ std::string join_vector(const std::vector<std::string>& v, const std::string& se
   return result;
 }
 
-std::vector<std::string> split_string(std::string&& s, const std::string& sep) {
+std::vector<std::string> split_string(std::string_view s) {
   std::vector<std::string> results;
+  for (char ch : s) {
+    results.emplace_back(std::string(1, ch));
+  }
+  return results;
+}
 
+std::vector<std::string> split_string(std::string_view s, std::string_view sep) {
+  if (sep.empty()) {
+    return split_string(s);
+  }
+
+  std::vector<std::string> results;
   while (s.size() > 0) {
     size_t pos = s.find(sep);
     results.emplace_back(s.substr(0, pos));
-    s.erase(0, pos);
-    s.erase(0, sep.size());
+    if (pos == std::string_view::npos) {
+      break;
+    }
+    s = s.substr(pos + sep.size());
   }
-
   return results;
 }
 
 }  // namespace fable
-
