cadd83ed61613ae6c3127abbcd589fffe7d20b3d
Benjamin Morgan <benjamin.morgan@bosch.com>
2023-07-25 09:15:26 +0200

    cli: Improve error handling of patch_rpath

--- cli/cloe_launch/utility.py
+++ cli/cloe_launch/utility.py
@@ -32,7 +32,7 @@ def run_cmd(
     return result
 
 
-def patch_rpath(file: Path, rpath: List[str]):
+def patch_rpath(file: Path, rpath: List[str]) -> Optional[subprocess.CompletedProcess]:
     """Set the RPATH of an executable or library.
 
     This will fail silently if the file is not an ELF executable.
@@ -41,15 +41,16 @@ def patch_rpath(file: Path, rpath: List[str]):
     assert file.exists()
 
     # Get original RPATH of file, or fail.
+    cmd = ["patchelf", "--print-rpath", str(file)]
     result = subprocess.run(
-        ["patchelf", "--print-rpath", str(file)],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        check=False,
+        cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=False
     )
-    if result.stderr.decode().strip() == "not an ELF executable":
-        # Silently ignore binaries that don't apply.
-        return
+    if result.returncode != 0:
+        if result.stderr.decode().strip() == "not an ELF executable":
+            # Silently ignore binaries that don't apply.
+            return None
+        logging.error(f"Error running: {' '.join(cmd)}")
+        logging.error(result.stderr)
     original = result.stdout.decode().strip()
     if original != "":
         rpath.append(original)
@@ -57,10 +58,7 @@ def patch_rpath(file: Path, rpath: List[str]):
     file_arg = str(file)
     rpath_arg = ":".join(rpath)
     logging.debug(f"Setting RPATH: {file_arg} -> {rpath_arg}")
-    subprocess.run(
-        ["patchelf", "--set-rpath", rpath_arg, file_arg],
-        check=True,
-    )
+    return run_cmd(["patchelf", "--set-rpath", rpath_arg, file_arg], must_succeed=False)
 
 
 def find_binary_files(cwd: Optional[Path] = None) -> List[Path]:
@@ -75,12 +73,10 @@ def find_binary_files(cwd: Optional[Path] = None) -> List[Path]:
     )
     return [Path(x) for x in result.stdout.decode().splitlines()]
 
+
 def patch_binary_files_rpath(src: Path, rpath: List[str]):
     """Patch RPATH all binary files found in src directory."""
     for file in find_binary_files(src):
         file = src / file
-        try:
-            logging.info(f"Patching RPATH: {file}")
-            patch_rpath(file, rpath)
-        except:
-            logging.warning(f"Error: cannot set RPATH of {file}")
+        logging.info(f"Patching RPATH: {file}")
+        patch_rpath(file, rpath)
