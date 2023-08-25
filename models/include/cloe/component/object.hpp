b7be566640c5e827df833e2965846785ad3e46e0
Martin Henselmeyer <martin.henselmeyer@bosch.com>
2023-08-25 15:36:06 +0200

    models(lua): Add Lua binding for Object

--- models/include/cloe/component/object.hpp
+++ models/include/cloe/component/object.hpp
@@ -24,6 +24,8 @@
 #include <memory>  // for shared_ptr<>
 #include <vector>  // for vector<>
 
+#include <sol/sol.hpp>  // for Lua related aspects
+
 #include <Eigen/Geometry>           // for Isometry3d, Vector3d
 #include <fable/enum.hpp>           // for ENUM_SERIALIZATION
 #include <fable/json.hpp>           // for Json
@@ -97,6 +99,19 @@ struct Object {
         {"acceleration", o.acceleration},
         {"angular_velocity", o.angular_velocity},
     };
+  }
+  friend void to_lua(sol::state_view view, Object* /* value */) {
+    sol::usertype<Object> usertype_table = view.new_usertype<Object>("Object");
+    usertype_table["id"] = &Object::id;
+    usertype_table["exist_prob"] = &Object::exist_prob;
+    usertype_table["type"] = &Object::type;
+    usertype_table["classification"] = &Object::classification;
+    usertype_table["pose"] = &Object::pose;
+    usertype_table["dimensions"] = &Object::dimensions;
+    usertype_table["cog_offset"] = &Object::cog_offset;
+    usertype_table["velocity"] = &Object::velocity;
+    usertype_table["acceleration"] = &Object::acceleration;
+    usertype_table["angular_velocity"] = &Object::angular_velocity;
   }
 };
 
