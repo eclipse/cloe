1d2fcf413b212b302461879eb9867ad0ea71d878
Benjamin Morgan <benjamin.morgan@bosch.com>
2023-10-11 11:36:46 +0200

    engine(lua): Fix failing test_lua10_heavy_cpu.lua
    We now check that the achievable_realtime_factor is greater than 1
    to directly determine success or failure.

--- engine/tests/test_lua10_heavy_cpu.lua
+++ engine/tests/test_lua10_heavy_cpu.lua
@@ -1,7 +1,9 @@
 local cloe = require("cloe")
-local proj = require("project")
+local proj = cloe.require("project")
 
-proj.configure_all()
+proj.configure_all {
+    with_server = false,
+}
 proj.stop_after("1s")
 proj.set_realtime_factor(-1)
 
@@ -16,5 +18,16 @@ cloe.schedule {
             array[i] = math.random()
         end
         table.sort(array)
+    end
+}
+
+cloe.schedule {
+    on = "stop",
+    run = function(sync)
+        if sync:achievable_realtime_factor() > 1 then
+            cloe.scheduler.execute_action("succeed")
+        else
+            cloe.scheduler.execute_action("fail")
+        end
     end
 }
