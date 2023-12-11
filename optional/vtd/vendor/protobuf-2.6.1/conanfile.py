d3d090b4975511f4002c38e52dfee339c3455d69
Benjamin Morgan <benjamin.morgan@bosch.com>
2023-12-11 11:20:08 +0100

    vtd: Migrate protobuf library to Conan 2.0 compatibility

--- optional/vtd/vendor/protobuf-2.6.1/conanfile.py
+++ optional/vtd/vendor/protobuf-2.6.1/conanfile.py
@@ -2,8 +2,12 @@
 # pylint: skip-file
 
 import os
+import subprocess
 
-from conans import AutoToolsBuildEnvironment, ConanFile, tools
+from conan import ConanFile
+from conan.tools import files, gnu
+
+required_conan_version = ">=1.52.0"
 
 
 class ProtobufConan(ConanFile):
@@ -14,38 +18,97 @@ class ProtobufConan(ConanFile):
     description = "Google Protocol Buffers."
     topics = ("protobuf", "serialization")
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
-    build_policy = "missing"
-    _patch_v2p6p1 = "patches/protobuf_v2.6.1.patch"  # git format-patch v2.6.1 --stdout > protobuf_v2.6.1.patch
+    generators = "AutotoolsToolchain"
+    options = {
+        "lite": [True, False],
+    }
+    default_options = {
+        "lite": False,
+    }
     exports_sources = [
-        _patch_v2p6p1,
-        "CMakeLists.txt",
+        "patches/*",
+        "cmake/*",
     ]
     build_requires = [
         "autoconf/[>=2.69]",
         "libtool/[>=2.4.6]",
     ]
-    _src_dir = "protobuf"
-    _git_url = url
-    _git_ref = f"v{version}"
-    _git_dir = "protobuf"
-    _autotools = None
+
+    @property
+    def _is_clang_x86(self):
+        return self.settings.compiler == "clang" and self.settings.arch == "x86"
 
     def source(self):
-        git = tools.Git(folder=self._git_dir)
-        git.clone(self._git_url, branch=self._git_ref, shallow=True)
+        files.get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        for patch in self.conan_data["patches"][self.version]:
+            files.patch(self, patch_file=patch)
 
     def build(self):
-        # change googletest version
-        tools.patch(base_path=self._src_dir, patch_file=self._patch_v2p6p1)
-        self.run(f"cd {self._src_dir} && ./autogen.sh")
-        # configure and make
-        self._autotools = AutoToolsBuildEnvironment(self)
-        self._autotools.configure(configure_dir=self._src_dir)
-        self._autotools.make()
+        autotools = gnu.Autotools(self)
+        if self.should_configure:
+            autotools.configure()
+        if self.should_build:
+            autotools.make()
+
+    @property
+    def _cmake_install_base_path(self):
+        return os.path.join("lib", "cmake", "protobuf")
 
     def package(self):
-        self._autotools.make(target="install")
+        if not self.should_install:
+            return
+        autotools = gnu.Autotools(self)
+        autotools.install()
+
+        # files.copy(self, "*.proto",
+        files.copy(self, "cmake/*", dst=os.path.join(self.package_folder, self._cmake_install_base_path), src=self.source_folder, keep_path=False)
+
+        if not self.options.lite:
+            files.rm(self, "libprotobuf-lite*", os.path.join(self.package_folder, "lib"))
+            files.rm(self, "libprotobuf-lite*", os.path.join(self.package_folder, "bin"))
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.set_property("cmake_find_mode", "both")
+        self.cpp_info.set_property("cmake_module_file_name", "Protobuf")
+        self.cpp_info.set_property("cmake_file_name", "protobuf")
+        self.cpp_info.set_property("pkg_config_name", "protobuf_full_package") # unofficial, but required to avoid side effects (libprotobuf component "steals" the default global pkg_config name)
+
+        build_modules = [
+            os.path.join(self._cmake_install_base_path, "protobuf-generate.cmake"),
+            os.path.join(self._cmake_install_base_path, "protobuf-module.cmake"),
+            os.path.join(self._cmake_install_base_path, "protobuf-options.cmake"),
+        ]
+        self.cpp_info.set_property("cmake_build_modules", build_modules)
+
+        # libprotobuf
+        self.cpp_info.components["libprotobuf"].set_property("cmake_target_name", "protobuf::libprotobuf")
+        self.cpp_info.components["libprotobuf"].set_property("pkg_config_name", "protobuf")
+        self.cpp_info.components["libprotobuf"].includedirs = ["include"]
+        self.cpp_info.components["libprotobuf"].libs = ["protobuf"]
+        if self.settings.os in ["Linux", "FreeBSD"]:
+            self.cpp_info.components["libprotobuf"].system_libs.extend(["m", "pthread"])
+            if self._is_clang_x86 or "arm" in str(self.settings.arch):
+                self.cpp_info.components["libprotobuf"].system_libs.append("atomic")
+
+        # libprotoc
+        self.cpp_info.components["libprotoc"].set_property("cmake_target_name", "protobuf::libprotoc")
+        self.cpp_info.components["libprotoc"].libs = ["protoc"]
+        self.cpp_info.components["libprotoc"].requires = ["libprotobuf"]
+
+        # libprotobuf-lite
+        if self.options.lite:
+            self.cpp_info.components["libprotobuf-lite"].set_property("cmake_target_name", "protobuf::libprotobuf-lite")
+            self.cpp_info.components["libprotobuf-lite"].set_property("pkg_config_name", "protobuf-lite")
+            self.cpp_info.components["libprotobuf-lite"].includedirs = ["include"]
+            self.cpp_info.components["libprotobuf-lite"].libs = ["protobuf-lite"]
+            if self.settings.os in ["Linux", "FreeBSD"]:
+                self.cpp_info.components["libprotobuf-lite"].system_libs.extend(["m", "pthread"])
+                if self._is_clang_x86 or "arm" in str(self.settings.arch):
+                    self.cpp_info.components["libprotobuf-lite"].system_libs.append("atomic")
+
+        # TODO: Remove once everything is on Conan v2:
+        self.cpp_info.filenames["cmake_find_package"] = "Protobuf"
+        self.cpp_info.filenames["cmake_find_package_multi"] = "protobuf"
+        self.cpp_info.names["pkg_config"] ="protobuf_full_package"
+        self.env_info.LD_LIBRARY_PATH.append(os.path.join(self.package_folder, "lib"))
+        self.env_info.PATH.append(os.path.join(self.package_folder, "bin"))
---
+++ optional/vtd/vendor/protobuf-2.6.1/test_package/conanfile.py
@@ -0,0 +1,34 @@
+from conan import ConanFile
+from conan.tools.build import can_run
+from conan.tools.cmake import cmake_layout, CMake, CMakeToolchain
+import os
+
+
+class TestPackageConan(ConanFile):
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "CMakeDeps", "VirtualBuildEnv", "VirtualRunEnv"
+    test_type = "explicit"
+
+    def layout(self):
+        cmake_layout(self)
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
+
+    def build_requirements(self):
+        self.tool_requires(self.tested_reference_str)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.cache_variables["protobuf_LITE"] = self.dependencies[self.tested_reference_str].options.lite
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        if can_run(self):
+            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
+            self.run(bin_path, env="conanrun")
