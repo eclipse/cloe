a498b7993368c586a73dbfc864c263f3d8383b5a
Moritz Hoffmann <moritz.hoffmann2@bosch.com>
2024-02-08 10:37:08 +0100

    python: implemented python function

---
+++ python/src/cxx/wrapper/python_function.cpp
@@ -0,0 +1,22 @@
+//
+// Created by ohf4fe on 08.02.24.
+//
+#include "python_function.hpp"
+
+#include <utility>
+#include <memory>
+
+namespace cloe::py {
+PythonFunction::PythonFunction(CallbackFunction py_fun, std::string_view name)
+    : cloe::Action(std::string(name)), py_fun_(std::move(py_fun)) {}
+ActionPtr PythonFunction::clone() const { return std::make_unique<PythonFunction>(*this); }
+CallbackResult PythonFunction::operator()(const Sync& sync, TriggerRegistrar& /*registrar*/) {
+  try {
+    return py_fun_(sync);
+  } catch (const std::exception &e) {
+    throw cloe::Error("error executing Python function: {}", e.what());
+  }
+}
+void PythonFunction::to_json(Json& j) const { j = {}; }
+
+}
---
+++ python/src/cxx/wrapper/python_function.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "cloe/conf/action.hpp"
+#include <functional>
+#include <string_view>
+
+namespace cloe::py {
+
+class PythonFunction : public cloe::Action {
+ public:
+  using CallbackFunction = std::function<CallbackResult(const cloe::Sync&)>;
+  PythonFunction(CallbackFunction py_fun, std::string_view name);
+  PythonFunction(const PythonFunction &) = default;
+  PythonFunction &operator=(const PythonFunction&) = default;
+  PythonFunction(PythonFunction &&) = default;
+  PythonFunction &operator=(PythonFunction&&) = default;
+  ~PythonFunction() override = default;
+
+  [[nodiscard]] ActionPtr clone() const override;
+
+  CallbackResult operator()(const Sync &sync, TriggerRegistrar &registrar) override;
+  void to_json(Json &j) const override;
+
+ private:
+  CallbackFunction py_fun_;
+};
+
+}
