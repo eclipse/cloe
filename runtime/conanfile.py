4404cabdec8c0f8130efd7997e28a3ba9dca63e4
Moritz Hoffmann <moritz.hoffmann2@bosch.com>
2024-04-23 10:41:19 +0200

    cloe runtime: require pybind

--- runtime/conanfile.py
+++ runtime/conanfile.py
@@ -50,6 +50,7 @@ class CloeRuntime(ConanFile):
         self.requires("spdlog/1.11.0")
         self.requires("incbin/cci.20211107")
         self.requires("sol2/3.3.1")
+        self.requires("pybind11/2.10.1")
 
     def build_requirements(self):
         self.test_requires("gtest/1.13.0")
